91ce519377cb0e64b64854db39de13b69745e3c2
Mike Grilli <mikeg@downtownseattle.org>
2023-11-17 17:38:37 -0800

    Updated assignment

--- carProto.js
+++ carProto.js
@@ -6,6 +6,25 @@
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
 
+class Car {
+    constructor(model) {
+        this.model = model
+        this.currentSpeed = 0
+    }
+
+    accelerate() {
+        this.currentSpeed ++
+    }
+
+    break() {
+        this.currentSpeed --
+    }
+
+    toString() {
+        return `Car Model ${this.model} current speed is ${this.currentSpeed}`
+    }
+}
+
 /**
  * ElectricCar class
  * @constructor
@@ -15,3 +34,8 @@
 
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const ford = new Car('Outback')
+ford.accelerate()
+// ford.break()
+console.log(ford.toString())
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -13,3 +13,36 @@
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+ 
+$(document).ready(function() {
+    //create a tag
+    
+    const $a = $('<a>')
+    $a.attr('id', 'cta')
+    $a.attr('href', '#')
+    $a.text('Buy Now!')
+
+    //append a tag to body after last p
+    const $lastP = $('p')
+    $a.insertAfter($lastP);
+
+    //access data-color atrr of the image
+    const img = $('img')
+    console.log(img[0].dataset.color)
+
+    //highloght 3rd list item
+    const $list = $('li')
+    $list.eq(2).addClass('highlight')
+
+    //remove last paragraph
+    $lastP.remove()
+
+    //on click remove  link and add text where the button was
+    $a.on('click', (e) => {
+        $a.remove()
+        const $newP = $('<p>')   
+        $newP.text('Added to cart')
+        $('main').append($newP)
+    })
+
+})
