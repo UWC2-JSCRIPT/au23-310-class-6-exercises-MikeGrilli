bda65fb4f8cf051bc26c3fb8d76364f88960cb71
Mike Grilli <mikeg@downtownseattle.org>
2023-11-18 21:33:42 -0800

    Update Assignment

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,47 @@
-/**
- * Toggles "done" class on <li> element
- */
+$(document).ready(function() {
+  /**
+   * Toggles "done" class on <li> element
+   */
 
-/**
- * Delete element when delete link clicked
- */
+  function toggleItem(e) {
+    $(this).toggleClass("done");
+  }
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+  $('li').on('click', toggleItem)
 
-  // rest here...
-};
+  /**
+   * Delete element when delete link clicked
+   */
 
-// add listener for add
+  function deleteItem() {
+    $(this).parent().fadeOut(500);
+  }
+
+  $('.delete').on('click', deleteItem)
+
+  /**
+   * Adds new list item to <ul>
+   */
+  const addListItem = function(e) {
+    e.preventDefault();
+
+    const text = $('input').val();
+    const $li = $('<li>')
+    const $span = $('<span>').text(text)
+    const $button = $('<a>').text('Delete').addClass('delete')
+    
+    $li.append($span)
+    $('ul').append($li)
+    $li.append($button)
+
+    $('li').on('click', toggleItem)
+    $('.delete').on('click', deleteItem)
+
+  }
+
+  // add listener for add
+
+  $('.add').on('click', addListItem)
+
+})
+
--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,30 @@
 // Do not change
-document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+$(document).ready(function () {
+  
+  document.getElementById('cat').addEventListener('click', () => {
+    alert('meow!');
+  });
+
+  // When clicked, "More info" link should alert "Here's some info"
+  // instead of going to a new webpage
+
+  const $moreInfo = $('#more-info')
+
+  $moreInfo.on('click', function(e) {
+    e.preventDefault()
+  })
+  // When the bark button is clicked, should alert "Bow wow!"
+  // Should *not* alert "meow"
+  
+  const $dog = $('#dog')
+
+  $dog.on('click', function(e) {
+    e.preventDefault()
+    alert('Bow Wow!')
+    e.stopPropagation()
+  })
+
 });
 
-// When clicked, "More info" link should alert "Here's some info"
-// instead of going to a new webpage
 
-// When the bark button is clicked, should alert "Bow wow!"
-// Should *not* alert "meow"
+
